226a7a2958b4c930e42082bb525cb5714cd24f04
LeeKangRyong <gaiogo2@naver.com>
2025-08-27 14:44:46 +0900

    profile API 연결

--- src/models/profile.js
+++ src/models/profile.js
@@ -49,3 +49,93 @@ export const getProfileMissions = async accessToken => {
 
   return res.data;
 };
+
+// 구성원 조회
+export const getMembers = async accessToken => {
+  if (useMock) {
+  }
+
+  const res = await axios.get(`${baseUrl}/family/members`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    withCredentials: true,
+  });
+
+  return res.data;
+  /*
+    RESPONSE
+    /*
+    [
+      {
+      "id":1,
+      "member_id":4,
+      "member_name":"아들",
+      "member_image":"xxx.jpg",
+      "member_gender":"남",
+      "member_age":10
+      },
+      ...
+    ]
+    */
+};
+
+// 구성원 생성
+export const createMembers = async accessToken => {
+  if (useMock) {
+  }
+
+  const res = await axios.post(`${baseUrl}/family/members`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    withCredentials: true,
+  });
+
+  return res.data;
+
+
+  /*
+    REQUEST
+    multipart/form-data - member_name, member_image, member_gender?, member_age?
+  */
+
+  /*
+    RESPONSE
+    { "message": "구성원 등록 완료", member_image_url  : “….” }
+  */
+};
+
+// 구성원 삭제
+export const deleteMembers = async (member_id, accessToken) => {
+  if (useMock) {
+  }
+
+  const res = await axios.delete(`${baseUrl}/family/members/${member_id}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    withCredentials: true,
+  });
+
+  return res.data;
+  /*
+    RESPONSE
+    { "message": "구성원 삭제 완료" }
+  */
+};
+
+// 구성원 수정
+export const updateMembers = async (member_id, accessToken) => {
+  if (useMock) {
+  }
+
+  const res = await axios.put(`${baseUrl}/family/members/${member_id}`, {
+    headers: { Authorization: `Bearer ${accessToken}` },
+    withCredentials: true,
+  });
+
+  return res.data;
+  /*
+    REQUEST
+    multipart/form-data - member_name, member_image, member_gender?, member_age?
+
+    RESPONSE
+    { "message": "구성원 정보 수정 완료", member_image_url  : “….” }
+  */
+};
+
